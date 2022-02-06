6c6abac2c3370fb6bf4664959d8ade8ffca5c131
python <python290821@gmail.com>
2022-02-06 20:44:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
+import pika
 
-print('hello')
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+channel.queue_declare(queue='hello')
+
+channel.basic_publish(exchange='',
+                      routing_key='hello',
+                      body='Hello World!')
+print(" [x] Sent 'Hello World!'")
