4021d25e8658223e691ea08383ed63e5f628c5d6
python <python290821@gmail.com>
2022-02-06 19:49:21 +0200

    Update ConnectionPoolSingleton.py

--- ConnectionPoolSingleton.py
+++ ConnectionPoolSingleton.py
@@ -13,35 +13,13 @@ class ConnectionPoolSingleton(object):
     def get_instance(cls):
         if cls._instance:
             return cls._instance
-        with cls._lock:
+        cls._lock.acquire()
+        try:
             if cls._instance is None: #t1 + t2
                 cls._instance = cls.__new__(cls)
                 cls._instance.connections = [MyConnection(i) for i in range(cls._max_connections)]
-            return cls._instance
-
-    def get_free_count(self):
-        return len(self.connections)
-
-    def get_max_possible_connections(self):
-        return ConnectionPoolSingleton._max_connections
-
-    def get_connection(self):
-        # will return a connection and remove it from the list
-        # return self.connections ...
-        # lock
-        pass
-
-    def return_connection(self, conn):
-        # will take the connection and add it to the list
-        # self.connections --> append
-        # lock
-        pass
+        finally:
+            cls._lock.release()
+        return cls._instance
 
 
-sing1 = ConnectionPoolSingleton.get_instance()
-sing2 = ConnectionPoolSingleton.get_instance()
-print(sing1 == sing2)
-print(sing1)
-print(sing2)
-print(sing1.name)
-sing1.print_hello()
